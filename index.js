5770d47625ec3e2a34ee28ffedf629b3dc5b4320
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-05-19 15:45:07 -0500

    May 19 2023  Deleted rest of code in comments from old code, working on search bar

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ console.log("javascript file connected");
 
 document.getElementById('recipe').addEventListener('submit', (e) => {
     e.preventDefault()
-    console.log('Help')
     fetchRecipeList()
     }
 )
@@ -56,14 +55,7 @@ function fetchRecipeList(){
 
 
 
-/** document.addEventListener('DOMContentLoaded', function(){
-    const recipe = document.getElementById("recipe");
-    recipe.addEventListener("submit", (e) => {
-        e.preventDefault();
-        recipeTest()
-    })
-    recipe.reset();
-    const recipeToAdd = document.getElementById("enterRec");
+const recipeToAdd = document.getElementById("enterRec");
     recipeToAdd.addEventListener("submit", (event) => { 
     event.preventDefault();
     let newAddRecipe = {
@@ -71,14 +63,14 @@ function fetchRecipeList(){
         credit: event.target.website.value,
     }
     newRecipe(newAddRecipe)
-    });
-});**/
+    })
 
 
 
 
 
-/**function newRecipe(newAddRecipe){
+
+function newRecipe(newAddRecipe){
         fetch ('http://localhost:3000/Recipes', {
             method: 'POST',
             headers: {
@@ -90,39 +82,4 @@ function fetchRecipeList(){
         .then(recipe => console.log(recipe))
 }
 
-function recipeFilter(){
-}
 
-function recipeSection (completeRecipe){
-    const main = document.getElementById('recipeContainer');
-    completeRecipe.forEach(recipe => {
-        const recipeCard = document.createElement('div');
-        const h2 = document.createElement('h2');
-        const li = document.createElement('li');
-        const img = document.createElement('img');
-
-        recipeCard.class = 'recipes';
-        h2.innerHTML = recipe.meal;
-        li.innerHTML = recipe.mainIngredients;
-
-        img.src = recipe.recipeImage;
-       
-       
-        recipeCard.append(h2,li,img);
-        main.append(recipeCard);
-    })
-    
-}
-
-function returnIngredients(ingredients){
-  
-   
-}
-
-function recipeTest() {
-
-    const recipeArray = fetch ('http://localhost:3000/Recipes')
-        .then(resp => resp.json())
-        .then(json => recipeSection(json))
-    return recipeArray
-}**/
